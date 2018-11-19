c9b5aa1229a4d4b70ac68edbab5947b6b65d8669
Paula Muldoon <pmuldoon@umich.edu>
2018-11-19 16:37:54 +0000

    update cocoajs to v. 2.0.2

--- test/Calculator.js
+++ test/Calculator.js
@@ -1,13 +1,44 @@
 const CocoaJS = require('cocoajs');
-const add = require('./params/add');
-
 
 describe('test eq method', () => {
     describe('addition method', () => {
+        const setup = {
+            codeFile: '../../../src/Calculator',
+            methodName: 'add',
+            it: 'should add 2 numbers',
+        };
+
+        const scenarios = {
+            scenario1: {
+                params: {
+                    number1: 1,
+                    number2: 2,
+                },
+                message: 'numbers to add',
+                expected: 3,
+            },
+
+            scenario2: {
+                params: {
+                    number1: 2,
+                    number2: 2,
+                },
+                message: 'numbers to add',
+                expected: 4,
+            },
+
+            scenario3: {
+                params: {
+                    number1: 4,
+                    number2: 10,
+                },
+                message: 'numbers to add',
+                expected: 14,
+            },
+        };
         CocoaJS.eq(
-            add.scenarios(),
-            add.setup(),
-            add.setup().paramsFilePath,
+            setup,
+            scenarios
         );
     });
 });
--- test/params/add.js
+++ test/params/add.js
@@ -1,43 +1,7 @@
 class add{
-    static setup() {
-        return {
-            codeFile: '../../../src/Calculator',
-            paramsFilePath: '../../../test/params/add',
-            methodName: 'add',
-            it: 'should add 2 numbers',
-        };
-    }
-
-    static scenarios() {
-        return {
-            scenario1: {
-                params: {
-                    number1: 1,
-                    number2: 2,
-                },
-                message: 'numbers to add',
-                expected: 3,
-            },
-
-            scenario2: {
-                params: {
-                    number1: 2,
-                    number2: 2,
-                },
-                message: 'numbers to add',
-                expected: 4,
-            },
-
-            scenario3: {
-                params: {
-                    number1: 4,
-                    number2: 10,
-                },
-                message: 'numbers to add',
-                expected: 14,
-            },
-        }
-    }
+    // static scenarios() {
+    //     return
+    // }
 }
 
 module.exports = add;
